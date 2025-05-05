4b5dcda31cdf87990d6e99e8050c9c2175c76b85
Imad Manni <128246543+ImadManni@users.noreply.github.com>
2025-05-05 11:53:53 +0100

    Update firebase.config.ts

--- firebase.config.ts
+++ firebase.config.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvnKWFE5FzpV1MhrZzbF5eoR3yUHuEBhs",
-  authDomain: "taddart-money-tracker.firebaseapp.com",
-  projectId: "taddart-money-tracker",
-  storageBucket: "taddart-money-tracker.appspot.com",
-  messagingSenderId: "246597599441",
-  appId: "1:246597599441:web:a85775206971648d5005f0",
-  measurementId: "G-01VCBTWDDY",
+  apiKey: "***************",
+  authDomain: "**********",
+  projectId: "***********",
+  storageBucket: "************************",
+  messagingSenderId: "*********",
+  appId: "******************************",
+  measurementId: "********",
 };
 
 const app = initializeApp(firebaseConfig);
